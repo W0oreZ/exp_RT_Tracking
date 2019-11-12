31729b11e07c7a8dc49a5154c2a3091890ae634c
W0oreZ <elkhadiri.abderrazak@gmail.com>
2019-11-12 12:36:08 +0100

    blink task

--- src/Blink/v2x_blink.cpp
+++ src/Blink/v2x_blink.cpp
@@ -1,22 +1,24 @@
-#include "src/Blink/v2x_blink.h"
-#include <Arduino.h>
-#include <STM32FreeRTOS.h>
+#include "v2x_blink.h"
+
+//STATE VARIABLES
+int bInterval; //in seconds
 
 void V2X_BLINK::setup()
 {
-  Serial.println("BLINK TASK Started !");
   pinMode(LED_NET, OUTPUT);
+  bInterval = 2;
 }
 
 void V2X_BLINK::loop()
 {
   digitalWrite(LED_NET, HIGH);
-  vTaskDelay(500 / portTICK_PERIOD_MS );
+  vTaskDelay((2*1000)/portTICK_PERIOD_MS);
   digitalWrite(LED_NET, LOW);
-  vTaskDelay(500 / portTICK_PERIOD_MS );
+  vTaskDelay((2*1000)/portTICK_PERIOD_MS);
 }
 
 void V2X_BLINK::main(void* p){
+  Serial.println("Blink TASK Started !");
   V2X_BLINK::setup();
 
   for(;;)
--- src/Blink/v2x_blink.h
+++ src/Blink/v2x_blink.h
@@ -1,12 +1,17 @@
 #ifndef v2x_blink_h
 #define v2x_blink_h
 
-#define LED_NET PB3 
+#include <Arduino.h>
+#include <STM32FreeRTOS.h>
+#include "STM32FreeRTOSConfig.h"
+
+#define LED_NET PB3
 
 namespace V2X_BLINK
 {
-  void main(void* p);
 
+  //TASK CORE FUNCTIONS
+  void main(void* p);
   void setup();
   void loop();
 }
