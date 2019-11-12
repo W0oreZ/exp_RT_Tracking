371d995b74494306ec1adf8cb0c04ca88ef37423
W0oreZ <elkhadiri.abderrazak@gmail.com>
2019-11-12 12:36:36 +0100

    free rtos config

--- globals.h
+++ globals.h
@@ -1,9 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
 #include <Arduino.h>
 #include <STM32FreeRTOS.h>
-
-#define LED_NET PB3
+#include "STM32FreeRTOSConfig.h"
 
 HardwareSerial Serial2(PA3, PA2);
 
-TaskHandle_t v2x_network_handler = NULL;
-TaskHandle_t v2x_blink_handler = NULL;
+#endif
