a890c708ef50e1db974a8d5c902c917b98c743b5
W0oreZ <elkhadiri.abderrazak@gmail.com>
2019-12-16 13:51:11 +0100

    spieeprom-test

---
+++ src/vendor/eeprom/spieeprom/spieeprom.cpp
@@ -0,0 +1,53 @@
+#include "spieeprom.h"
+
+SPIEEPROM::SPIEEPROM() {
+	eeprom_type = 0;
+}
+
+SPIEEPROM::SPIEEPROM(byte type) {
+	if (type>1) {
+		eeprom_type = 0;
+	} else {
+		eeprom_type = type;
+	}
+}
+
+void SPIEEPROM::begin() {
+	pinMode(SLAVESELECT, OUTPUT);
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0); 
+    SPI.setClockDivider(SPI_CLOCK_DIV64);
+}
+
+void SPIEEPROM::send_address(long addr) {
+	if (eeprom_type == 1) SPI.transfer((byte)(addr >> 16));	
+  	SPI.transfer((byte)(addr >> 8));
+  	SPI.transfer((byte)(addr));	
+}
+
+void SPIEEPROM::start_write() {
+	digitalWrite(SLAVESELECT,LOW);
+	SPI.transfer(WREN); //send WREN command
+	digitalWrite(SLAVESELECT,HIGH);
+	digitalWrite(SLAVESELECT,LOW);
+	SPI.transfer(WRITE); //send WRITE command
+}
+
+bool SPIEEPROM::isWIP() {
+	byte data;
+	digitalWrite(SLAVESELECT,LOW);
+	SPI.transfer(RDSR); // send RDSR command
+	data = SPI.transfer(0xFF); //get data byte
+	digitalWrite(SLAVESELECT,HIGH);
+	return (data & (1 << 0));
+}
+
+
+
+
+
+
+
+
+
